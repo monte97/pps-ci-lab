173ecbdd74531638844112e99890092a5aa96722
Francesco Montelli <francesco.montelli@studio.unibo.it>
2020-05-04 15:41:11 +0200

    test java

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,17 @@ plugins {
 	scala
 }
 
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
 	implementation("org.scala-lang:scala-library:2.12.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
 }
